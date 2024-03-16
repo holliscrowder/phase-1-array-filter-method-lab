3ceedcb8ae6c269f45970f8f7409d64033c07cf2
Hollis Crowder <hollis.crowder@gmail.com>
2024-03-16 16:09:01 -0700

    complete filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+// return all drivers who match names (not case-sensitive)
+function findMatching(drivers, checkName) {
+  return drivers.filter((driver) => {
+    return driver.toLowerCase() === checkName.toLowerCase();
+  });
+}
+
+// fuzzy match
+function fuzzyMatch(drivers, checkName) {
+  return drivers.filter((driver) => {
+    return driver.substring(0, checkName.length) === checkName;
+  });
+}
+
+// name match object
+function matchName(drivers, checkName) {
+  return drivers.filter((driver) => {
+    return driver.name === checkName;
+  });
+}
